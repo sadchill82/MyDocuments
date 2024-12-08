8927af2820588417abf7ae278be78d496866a2e9
Islam Nurmuhametov <nurmuhametoffislam@yandex.ru>
2024-12-09 00:55:02 +0300

    Initial Commit

---
+++ MyDocuments/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyDocuments
+//
+//  Created by Ислам on 09.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
